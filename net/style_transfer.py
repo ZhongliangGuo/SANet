dbac021b871667f801e423265d5ce3b1150f7695
Zhongliang Guo <93882634+ZhongliangGuo@users.noreply.github.com>
2024-05-22 15:52:04 +0100

    Add files via upload

--- net/style_transfer.py
+++ net/style_transfer.py
@@ -1,5 +1,8 @@
+from typing import List
+
 import torch
 import torch.nn as nn
+from torch import Tensor
 
 
 def adjust_learning_rate(optimizer, iteration_count, start_lr, lr_decay):
@@ -8,13 +11,13 @@ def adjust_learning_rate(optimizer, iteration_count, start_lr, lr_decay):
         param_group['lr'] = lr
 
 
-def calc_mean_std(tensor: torch.Tensor, eps=1e-5):
+def calc_mean_std(tensor: Tensor, eps=1e-5):
     mean = tensor.mean(dim=[2, 3], keepdim=True)
     std = (tensor.var(dim=[2, 3], keepdim=True) + eps).sqrt()
     return mean, std
 
 
-def normalize(tensor: torch.Tensor, eps=1e-5):
+def normalize(tensor: Tensor, eps=1e-5):
     mean, std = calc_mean_std(tensor, eps)
     return (tensor - mean) / std
 
@@ -129,7 +132,7 @@ class SANet(nn.Module):
         self.h = nn.Conv2d(in_channels=in_channels, out_channels=in_channels, kernel_size=1)
         self.out = nn.Conv2d(in_channels=in_channels, out_channels=in_channels, kernel_size=1)
 
-    def forward(self, content, style):
+    def forward(self, content: Tensor, style: Tensor):
         assert content.shape == style.shape
         B, C, H, W = content.shape
         return self.out(torch.bmm(self.h(style).flatten(2, -1),
@@ -147,7 +150,7 @@ class Fusion(nn.Module):
         self.merge = nn.Sequential(nn.ReflectionPad2d((1, 1, 1, 1)),
                                    nn.Conv2d(in_channels, in_channels, 3))
 
-    def forward(self, content_feats, style_feats):
+    def forward(self, content_feats: List[Tensor], style_feats: List[Tensor]):
         return self.merge(
             self.sanet4_1(content_feats[-2], style_feats[-2]) +
             self.upsample5_1(self.sanet5_1(content_feats[-1], style_feats[-1]))
@@ -155,7 +158,7 @@ class Fusion(nn.Module):
 
 
 class StyleTransfer(nn.Module):
-    def __init__(self, fusion, encoder, decoder):
+    def __init__(self, fusion: nn.Module, encoder: nn.Module, decoder: nn.Module):
         super().__init__()
         self.fusion = fusion
         self.encoder = encoder
@@ -171,45 +174,52 @@ class StyleTransfer(nn.Module):
                 param.requires_grad = False
         self.mse_loss = nn.MSELoss()
 
-    def encode_with_intermediate(self, tensor):
+    def encode_with_intermediate(self, tensor: Tensor):
         results = [tensor]
         for i in range(5):
             func = getattr(self, 'enc_{:d}'.format(i + 1))
             results.append(func(results[-1]))
         return results[1:]
 
-    def encode(self, tensor):
+    def encode(self, tensor: Tensor):
         for i in range(5):
             tensor = getattr(self, 'enc_{:d}'.format(i + 1))(tensor)
         return tensor
 
-    def calc_content_loss_once(self, pred, target):
+    def calc_content_loss_once(self, pred: Tensor, target: Tensor):
         assert (pred.size() == target.size())
         # assert (target.requires_grad is False)
         return self.mse_loss(pred, target)
 
-    def calc_content_loss(self, preds, targets):
+    def calc_content_loss(self, preds: List[Tensor], targets: List[Tensor]):
         assert len(preds) == len(targets)
         loss_s = self.calc_content_loss_once(preds[0], targets[0])
         for i in range(1, 5):
             loss_s += self.calc_content_loss_once(preds[i], targets[i])
         return loss_s
 
-    def calc_style_loss_once(self, pred, target):
+    def calc_style_loss_once(self, pred: Tensor, target: Tensor):
         assert (pred.size() == target.size())
         # assert (target.requires_grad is False) # first, make sure which one requires gradients and which one does not.
         pred_mean, pred_std = calc_mean_std(pred)
         target_mean, target_std = calc_mean_std(target)
         return self.mse_loss(pred_mean, target_mean) + self.mse_loss(pred_std, target_std)
 
-    def calc_style_loss(self, preds, targets):
+    def calc_style_loss(self, preds: List[Tensor], targets: List[Tensor]):
         assert len(preds) == len(targets)
         loss_s = self.calc_style_loss_once(preds[0], targets[0])
         for i in range(1, 5):
             loss_s += self.calc_style_loss_once(preds[i], targets[i])
         return loss_s
 
-    def forward(self, content, style):
+    @torch.no_grad()
+    def inference(self, content: Tensor, style: Tensor):
+        content_feats = self.encode_with_intermediate(content)
+        style_feats = self.encode_with_intermediate(style)
+        gt = torch.clamp(self.decoder(self.fusion(content_feats, style_feats)), 0, 1)
+        return gt
+
+    def forward(self, content: Tensor, style: Tensor):
         content_feats = self.encode_with_intermediate(content)
         style_feats = self.encode_with_intermediate(style)
         gt = torch.clamp(self.decoder(self.fusion(content_feats, style_feats)), 0, 1)
